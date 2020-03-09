e5a4dad73d958c059fe643aa67510d98979a8838
Eugene Paryhin <eugene.paryhin@theorem.co>
2020-03-09 20:10:23 +0200

    qq

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Card = props => (
+    <div clasName="Card">
+        <h3>React for Designers</h3>
+        <p>12 sections</p>
+    </div>
+)
+
+export default Card
